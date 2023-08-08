b72b486560e629b0c0853f33b075f0c69272667c
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-08-08 10:11:35 +0700

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config(); //load env file
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT; //at (current process) in (env file) get (parameter port)
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
