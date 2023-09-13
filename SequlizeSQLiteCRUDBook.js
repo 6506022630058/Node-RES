56fe13230bee9a5c6814aaece27c0d6a7d3d5631
6506022630058 <s6506022630058@email.kmutnb.ac.th>
2023-09-13 09:58:18 +0700

    u

--- SequlizeSQLiteCRUDBook.js
+++ SequlizeSQLiteCRUDBook.js
@@ -103,5 +103,5 @@ app.delete('/books/:id', (req, res) => {
 });
 
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
